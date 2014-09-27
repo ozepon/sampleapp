75d4f8ef332ff4db3fd43386d12480c3470a4065
OzekiDaichi <dandy.buzz@gmail.com>
2014-09-27 12:59:23 +0900

    test add emailフォーマット

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+	# 存在性の入力チェック　→　presence
+	# 長さのチェック       →　length
+	# validates (:name, presence: true) とも記述できる
+	validates :name,  presence: true, length: { maximum: 50 }
+
+
+	# 	/			正規表現の開始を示す
+	# \A			文字列の先頭
+	# [\w+\-.]+		英数字、アンダースコア (_)、プラス (+)、ハイフン (-)、ドット (.) のいずれかを少なくとも1文字以上繰り返す
+	# @				アットマーク
+	# [a-z\d\-.]+	英小文字、数字、ハイフン、ドットのいずれかを少なくとも1文字以上繰り返す
+	# \.			ドット
+	# [a-z]+		英小文字を少なくとも1文字以上繰り返す
+	# \z			文字列の末尾
+	# /				正規表現の終わりを示す
+	# i				大文字小文字を無視するオプション
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+	
+end
