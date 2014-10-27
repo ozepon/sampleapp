6097fb4f90d7b8ff363ec07e2c371601cb07b18d
OzekiDaichi <dandy.buzz@gmail.com>
2014-10-27 13:06:26 +0900

    8.2.6 finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,8 @@ class SessionsController < ApplicationController
 		end
 	end
 
-	def delete
+	def destroy
+		sign_out
+		redirect_to root_url
 	end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,13 +10,13 @@ describe "Authentication" do
 		describe "with invalid information" do
 			before { click_button "Sign in" }
 
-			it { should have_content('Sign in') }
-			it { should have_selector('dev.alert.alert-error', test: 'Invalid') }
-		end
+			it { should have_title('Sign in') }
+			it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 
-		describe "after visiting another page" do
-			before { click_link "Home" }
-			it { should_not have_selector('div.alert.alert-error') }
+			describe "after visiting another page" do
+				before { click_link "Home" }
+				it { should_not have_selector('div.alert.alert-error') }
+			end
 		end
 
 		describe "with valid information" do
@@ -31,6 +31,11 @@ describe "Authentication" do
 			it { should have_link('Profile',     href: user_path(user)) }
 			it { should have_link('Sign out',    href: signout_path) }
 			it { should_not have_link('Sing in', href: signin_path) }
+			
+			describe "followed by signout" do
+				before { click_link "Sign out" }
+				it { should have_link('Sign in') }
+			end	
 		end
 	end
 end
